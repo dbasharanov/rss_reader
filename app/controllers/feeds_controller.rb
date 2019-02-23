42f5ea74bab6d8ecd16f793a9b93045c5de240b1
Dimitar Basharanov <dimitar@xenium.bg>
2019-02-23 16:07:35 +0200

    fix tests

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,5 +1,5 @@
 class FeedsController < ApplicationController
-  before_action :set_feed, only: %i[show edit update destroy]
+  before_action :set_feed, only: [:show, :edit, :update, :destroy]
 
   # GET /feeds
   # GET /feeds.json
--- spec/views/feeds/edit.html.erb_spec.rb
+++ spec/views/feeds/edit.html.erb_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe 'feeds/edit', type: :view do
-  it 'displays feed update form' do
+  it "displays feed update form" do
     assign(:feed, Feed.create(title: 'Sports News', url: 'http://feeds.reuters.com/reuters/sportsNews', id: 5))
 
     render
 
-    rendered.should have_selector('input#feed_title')
+    rendered.should have_selector("input#feed_title")
   end
 end
